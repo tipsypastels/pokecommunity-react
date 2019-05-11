55461d463078e7b41dcbca94035006a50dac2e32
Christos Giallouros <10516560+christos-giallouros@users.noreply.github.com>
2019-05-12 00:06:25 +0100

    spheal

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ThreadPage from './pages/ThreadPage';
+import Spheal from './pages/Spheal';
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <Route path ="/" exact component ={Spheal}/>
         <Route path="/thread/:threadid" exact component={ThreadPage}/>
       </Router>
     );
