eec6a438f6dc34d423ab249fbefcd93ef942df96
Nina <ninarachaebuie@gmail.com>
2019-07-28 00:40:52 -0500

    404 error page

--- src/scripts/partials/PagePartials/Errors/Err404.tsx
+++ src/scripts/partials/PagePartials/Errors/Err404.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import '../../../../styles/modules/PagePartials/Errors/Err404.scss';
-import ErrorPage from './ErrorPage';
+import Icon from '../../Icon';
 
 //TODO: link to error page doesn't seem to work, unknown if window.history.back() will actually pull back to the proper place.
 
@@ -10,14 +11,26 @@ export default class Err404 extends Component {
   render() {
     return (
       <Container fluid className="Err404">
-        <ErrorPage>
-          <div>
-            A 404 error has occured.
-             <div>
-              What you searched for doesn't seem to exist. Try <span onClick={() => window.history.back()}>going back</span> a page.
-             </div>
+        <div className="ErrorPage">
+          <Icon className="error-icon" name="exclamation-triangle" size="lg" fw />
+          <div className="error-subtitle">An error has occured!</div>
+          <div className="error-message">
+            <img src="https://i.imgur.com/9hUsWtN.png" alt="Red made an error!" className="error-image" />
+
+            <div className="error-text">
+              Red couldn't find the page you were looking for!
+                <div>
+                What you searched for doesn't seem to exist. Try <a className="link" onClick={() => window.history.back()}>going back</a> a page, or <a href="/search.php">search</a> for what you were looking for. If you think we should know what went wrong, feel free to <a href="/sendmessage.php">contact us</a>!
+                </div>
+            </div>
           </div>
-        </ErrorPage>
+
+          <Link to="/">
+            <Button variant="primary" className="error-home">
+              Home
+          </Button>
+          </Link>
+        </div>
       </Container>
     )
   }
--- src/scripts/partials/PagePartials/Errors/Err500.tsx
+++ src/scripts/partials/PagePartials/Errors/Err500.tsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
 import { Container } from 'react-bootstrap';
 
-import ErrorPage from './ErrorPage';
-
 export default class Err500 extends Component {
   render() {
     return (
-      <Container fluid className="Err404">
-        <ErrorPage>
-          <div>
-            A 500 error has occured.
-             <div>
-            </div>
-          </div>
-        </ErrorPage>
+      <Container fluid className="Err500">
+
       </Container>
     )
   }
