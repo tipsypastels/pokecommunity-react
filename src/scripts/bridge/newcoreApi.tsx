78cd8e3d4a0683cfcddb449e36d028b9da397364
tipsypastels <dakotasankey@gmail.com>
2019-09-23 22:59:52 -0700

    added mock api

--- src/scripts/bridge/newcoreApi.tsx
+++ src/scripts/bridge/newcoreApi.tsx
@@ -2,14 +2,44 @@ import React from 'react';
 
 import axios, { AxiosRequestConfig } from 'axios';
 import { apiUrl } from '../../configs/api.json';
+import { requestingMockData } from './devUtils';
+import MOCK_DATA from './MOCK_DATA.json';
 
 import Err404 from '../partials/PagePartials/Errors/Err404';
 import Err500 from '../partials/PagePartials/Errors/Err500';
 import Err503 from '../partials/PagePartials/Errors/Err503';
 
 export default function newcoreApi(params: AxiosRequestConfig) {
-  params.url = `${apiUrl}${params.url}`;
-  return axios(params);
+  let controllerUrl = params.url;
+  params.url = `${apiUrl}${controllerUrl}`;
+  
+  if (requestingMockData()) {
+    const data = findMockDataForUrl(controllerUrl);
+
+    return {
+      data,
+      status: 200,
+      mock: true,
+    };
+  } else {
+    return axios(params);
+  }
+}
+
+// each entry in this map is a key in the json file MOCK_DATA.json. the value is a regex against which the axios url will be matched 
+const MOCK_DATA_PROVIDERS = {
+  'notifications': /^\/notifications\/?$/,
+  'thread': /^\/threads\/\d+\/?/,
+}
+
+function findMockDataForUrl(url: string) {
+  for (let name in MOCK_DATA_PROVIDERS) {
+    if (MOCK_DATA_PROVIDERS[name].exec(url)) {
+      console.warn(`The request for ${url} returned an error. Returning preset mock ${name} data. This will not happen in development mode.`);
+
+      return MOCK_DATA[name];
+    }
+  }
 }
 
 export type NewcoreErrorCode = null | 404 | 500 | 503;
